31bb01cab58eb437a3a238cd0846fff2f47dc2d5
Mohamed Emara <marhjmal6@gmail.com>
2025-07-31 00:51:45 +0300

    some of edit in user router

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import * as userController from "../controllers/user.controllers.js";
+import * as userController from "../Controllers/user.controllers.js";
 
 const router = express.Router();
 
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
-
-//router.get("/:id", userController.getUserById);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.put("/:id", userController.updateUser);
 
 export default router;
